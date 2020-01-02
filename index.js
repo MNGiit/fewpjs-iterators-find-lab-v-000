9e9524c37d6bc04b2d3f2fab55eca5b4c249f5f6
Mohammed Noori <kmanoorib@gmail.com>
2020-01-02 04:00:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const superbowlWin = function(wins) {
   // return wins.find( w => w.result === "W");
   let found = wins.find( win => win.result === "W" )
   let year = undefined;
-  if !found {
+  if (!found) {
     return undefined;
   }
   else {
